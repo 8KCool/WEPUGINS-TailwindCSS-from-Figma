9ee0b705afa968e142cd9015997542d7e16d79c8
BlackPandalancer <8klancer@gmail.com>
2023-04-23 00:28:03 +0000

    conf-install package 'flowbite', config package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
+  content: [
+      "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
